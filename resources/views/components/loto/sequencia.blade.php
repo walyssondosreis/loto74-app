c27529c2322ea413a5a56193a2f3651837c35401
Walysson dos Reis <walyssondosreis@email.com>
2023-11-15 21:32:08 -0300

    Tentei colocar o livewire porem muito cru proxima etapa VueJS

--- resources/views/components/loto/sequencia.blade.php
+++ resources/views/components/loto/sequencia.blade.php
@@ -2,30 +2,32 @@
     $contx = 1;
 @endphp
 
-<div class="flex flex-wrap items-center w-full bg-blue-600">
-@foreach ($sequencias as $sq)
-    {{-- Elemento Geral --}}
-    <div class="flex items-center text-xs justify-center bg-red-500 flex-wrap">
-        {{-- Contador --}}
-        <div class="flex">
-        <div class="bg-yellow-300 p-1 w-6">
-            {{ $contx++ }}
-        </div>
-        {{-- Sequencia  --}}
-        <div class="bg-orange-400 flex border border-black p-1 w-20">
-            @foreach (explode(',', $sq['sequencia']) as $ns)
-                {{-- Digito --}}
-                <div class=" flex m-1 font-semibold">
-
-                    {{ $ns }}
+<div class="flex flex-wrap items-center justify-center border-2 border-roxo-claro rounded-lg">
+    @foreach ($sequencias as $sq)
+        {{-- Elemento Geral --}}
+        <div class="flex items-center text-xs justify-center flex-wrap">
+            {{-- Contador --}}
+            <div class="flex p-1">
+                <div class="left-0 p-1 w-8 flex align-middle text-center border border-gray-500 rounded-l-lg">
+                    {{ $contx++ }}º
                 </div>
-            @endforeach
+                {{-- Sequencia  --}}
+                <div class="flex border border-gray-500 p-1 w-20">
+
+                    @foreach (explode(',', $sq['sequencia']) as $ns)
+                        {{-- Digito --}}
+                        <div class=" flex m-1 font-semibold">
+
+                            {{ $ns }}
+                        </div>
+                    @endforeach
+                </div>
+                {{-- Quantidade --}}
+                <div class="text-center  writing-mode-vertical-right border border-gray-500 rounded-r-lg">
+                    {{ $sq['qtd'] }}
+                </div>
+            </div>
         </div>
-        {{-- Quantidade --}}
-        <div class="bg-blue-400 p-1 w-6">
-            {{ $sq['qtd'] }}
-        </div>
-    </div>
-    </div>
-@endforeach
+    @endforeach
 </div>
+
--- resources/views/loto.blade.php
+++ resources/views/loto.blade.php
@@ -5,18 +5,18 @@
     <x-notification :errors='$errors'></x-notification>
 
     <div class="flex flex-wrap">
-        <div class="hidden w-full p-4">
+        <div class="w-full p-4">
             {{-- Componente de formulário --}}
             <x-loto.formulario :filtros='$filtros'></x-loto.formulario>
         </div>
-        <div class="hidden flex flex-wrap lg:flex-nowrap md:flex-nowrap justify-center p-4">
+        <div class="flex flex-wrap lg:flex-nowrap md:flex-nowrap justify-center p-4">
             <div class="lg:p-4 md:p-4">
                 {{-- Componente analisador de concursos --}}
                 <x-loto.analisador :numeros='$numeros'></x-loto.analisador>
 
             </div>
             <div class="border-2 border-roxo-light rounded-lg p-4 ">
-                <div class="flex flex-wrap justify-center">
+                <div class="flex flex-wrap justify-evenly">
                     <?php foreach ($concursos as $cc) : ?>
                     {{-- Componente de bilhete --}}
                     @component('components.loto.bilhete')
@@ -28,15 +28,22 @@
                     <?php endforeach; ?>
                 </div>
 
-                <div class="flex justify-center pt-4">
+                <div class="flex justify-center pt-4 ">
                     {{ $concursos->links() }}
                 </div>
 
             </div>
         </div>
-        <div class="flex w-full">
+            {{-- <div class="flex w-full text-lg font-bold tracking-tight p-4 mt-4 text-gray-900 text-center">
+
+                SEQUÊNCIAS
+            </div> --}}
+        </div>
+
+        <div class="flex w-full justify-center p-4 pt-4 pb-4">
             {{-- Ranking de Sequencias --}}
-            <x-loto.sequencia :sequencias='$sequencias'></x-loto.sequencia>
+
+                <x-loto.sequencia :sequencias='$sequencias'></x-loto.sequencia>
         </div>
     </div>
 </x-page>
