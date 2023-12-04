343e11124886f392983020cc72b1f4f19fdca5d3
Walysson dos Reis <walyssondosreis@email.com>
2023-12-04 18:13:39 -0300

    Ajustando

--- resources/views/components/loto/formulario.blade.php
+++ resources/views/components/loto/formulario.blade.php
@@ -1,48 +1,55 @@
-{{--
-    Formulário Precisa:
-    - Action
-    - Nome do campo para ativar
-    --}}
-<div class="fixed top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+
+    {{-- class="fixed top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2" --}}
+<div class="p-4">
     {{-- Filtros de Jogos --}}
     <form action="{{ route('loto') }}" method="POST">
         @csrf
-        <div class="m-2 text-center justify-center p-4 border-2 border-gray-300 rounded-lg bg-white shadow-md">
-            <div class="justify-center mb-3 flex flex-wrap lg:flex-nowrap">
-                <div class="w-full m-2">
-                    <label class="form-label" for="concursos">Concurso:</label>
-                    <input type="text" class="form-input" id="concursos" name="concursos"
+        <div class="flex-col justify-center p-4 border border-black rounded-md">
+            {{-- Campos do formulario --}}
+            <div class="flex justify-center w-full gap-2 flex-wrap lg:flex-nowrap">
+                <div class="flex-col w-full">
+                    <label class="flex form-label " for="jogos">Numeros|Jogos|Apostas:</label>
+                    <input type="text" class="flex form-input border-black" id="jogos" name="jogos"
+                        placeholder="Ex.: 2204 ou 2204,2207,2306,n ou 2204-2209"
+                        >
+                </div>
+
+                <div class="flex-col w-full">
+                    <label class="flex form-label " for="concursos">Concurso:</label>
+                    <input type="text" class="flex form-input border-black" id="concursos" name="concursos"
                         placeholder="Ex.: 2204 ou 2204,2207,2306,n ou 2204-2209"
                         @if (!empty($filtros) && $filtros['concursos']) value="{{ $filtros['concursos'] }}"; @endif>
                 </div>
-                <div class="w-full m-2">
-                    <label class="form-label" for="sequencias">Sequência:</label>
-                    <input type="text" class="form-input" id="sequencias" name="sequencias"
+                <div class="flex-col w-full">
+                    <label class="flex form-label" for="sequencias">Sequência:</label>
+                    <input type="text" class="flex form-input border-black" id="sequencias" name="sequencias"
                         placeholder="Ex.: 33423 ou 33423,33324,33333,n"
                         @if (!empty($filtros) && $filtros['sequencias']) value="{{ $filtros['sequencias'] }}"; @endif>
 
                 </div>
-                <div class="lg:w-1/5 m-1">
-                    <label class="form-label" for="data_ini">Data de Início:</label>
-                    <input type="date" class="form-input" id="data_ini" name="data_ini"
+                <div class="flex-col">
+                    <label class="flex form-label" for="data_ini">Data de Início:</label>
+                    <input type="date" class="flex form-input border-black" id="data_ini" name="data_ini"
                         @if (!empty($filtros) && $filtros['data_ini']) value="{{ $filtros['data_ini'] }}"; @endif>
                 </div>
-                <div class="lg:w-1/5 m-1">
-                    <label class="form-label" for="data_fim">Data de Fim:</label>
-                    <input type="date" class="form-input" id="data_fim" name="data_fim"
+                <div class="flex-col">
+                    <label class="flex form-label" for="data_fim">Data de Fim:</label>
+                    <input type="date" class="flex form-input border-black" id="data_fim" name="data_fim"
                         @if (!empty($filtros) && $filtros['data_fim']) value="{{ $filtros['data_fim'] }}"; @endif>
 
                 </div>
             </div>
-            <div class="flex justify-center text-sm font-bold ">
+            {{-- Botões --}}
+            <div class="flex justify-center text-sm font-bold p-2 gap-2">
                 <button type="reset"
-                    class="p-6 m-1 bg-roxo-claro text-white py-2 rounded-lg shadow-2xl hover:bg-roxo-light duration-150">Cancelar</button>
+                    class="flex px-6 py-1 border border-black rounded-md">Cancelar</button>
                 <button type="submit"
-                    class="p-6 m-1 bg-roxo-claro text-white py-2 rounded-lg shadow-2xl hover:bg-roxo-light duration-150">Buscar</button>
+                    class="flex px-6 py-1 border border-black rounded-md">Buscar</button>
             </div>
-            <div class="flex justify-center p-2">
+            {{-- Em baixo botões --}}
+            <div class="flex justify-center">
 
-                <a class="p-1 rounded-md text-sm  hover:text-roxo-claro" href="{{ route('limparFiltros') }}">Limpar
+                <a class="flex text-xs" href="{{ route('limparFiltros') }}">Limpar
                     Filtros</a>
             </div>
 
