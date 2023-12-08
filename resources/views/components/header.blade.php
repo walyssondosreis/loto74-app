0b56eb87995edbc79920547597742198c3b1ba22
Walysson dos Reis <walyssondosreis@email.com>
2023-12-08 10:16:43 -0300

    Testando

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,6 +2,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $titulo }}</title>
-    @vite(['resources/css/app.css'])
+
 </head>
 
