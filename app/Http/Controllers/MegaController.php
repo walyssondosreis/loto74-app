d24aa4a4ca90faeb0c0c891fc016ed335b0c62cd
Walysson dos Reis <walyssondosreis@email.com>
2023-12-28 18:45:28 -0300

    Ajuste final mega

--- app/Http/Controllers/MegaController.php
+++ app/Http/Controllers/MegaController.php
@@ -15,7 +15,7 @@ class MegaController extends Controller
      */
     public function index()
     {
-        $resultado = '1-2-3-4-5-6';
+        $resultado = '';
         // Cria arquivo CSV
         $jogosCru = $this->carregarJogosMega();
         $jogosValidado = [];
