bac06b4e7d7936d3adeb9c8198f47c66d730be61
Walysson dos Reis <walyssondosreis@email.com>
2024-01-30 00:47:54 -0300

    Login ajustando

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -23,20 +23,17 @@ function login() {
 <template>
     <Head title="Login" />
     <div class="flex justify-center items-center bg-gradient-to-l from-gray-900 to-roxo-escuro h-screen">
-        <div class="flex px-6 drop-shadow-2xl lg:W-3/4 select-none">
-            <section class="w-1/2 hidden lg:block rounded-full bg-roxo-claro rounded-r-none ">
-
-                <img class="m-auto animate-balanco pointer-events-none"
-                    src="../../../../public/images/logo-loto/logo-loto-0.png" alt="">
-
-            </section>
-            <section class="text-center justify-center p-10 flex gap-6 rounded-lg bg-white lg:w-1/2 lg:rounded-l-none">
+        <div class="flex flex-wrap justify-center px-6 drop-shadow-2xl select-none">
+            <section class="text-center justify-center p-4 flex gap-6 rounded-md bg-white">
                 <form class="w-full" @submit.prevent="login">
                     <!-- @csrf Token CSRF para segurança -->
-                    <Logo />
+                    <div class="mb-4">
+
+                        <Logo/>
+                    </div>
                     <text-input v-model="form.email" :error="form.errors.email" class="p-2" label="Email" type="email"
                         autofocus autocapitalize="off" />
-                    <text-input v-model="form.password" :error="form.errors.password" class="p-2" label="Password"
+                    <text-input v-model="form.password" :error="form.errors.password" class="p-2" label="Senha"
                         type="password" />
 
                     <div class="flex p-2 mb-5">
