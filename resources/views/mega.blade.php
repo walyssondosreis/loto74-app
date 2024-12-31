700a3c2ee10b84e226f191896412a2fc83d5367d
Walysson dos Reis <38730743+walyssondosreis@users.noreply.github.com>
2024-12-31 15:13:19 -0300

    Update mega.blade.php

--- resources/views/mega.blade.php
+++ resources/views/mega.blade.php
@@ -21,7 +21,7 @@
         {{-- Titulo --}}
         <div class="flex justify-center bg-green-600 border-b border-green-800 ">
             <span class="flex text-2xl p-4 font-nexah text-center py-4 text-white">
-               <span class="text-yellow"> ÚLTIMO <span> <br> BOLÃO DA MEGA 2024/2025 <br> Concurso 2810
+               ÚLTIMO <br> BOLÃO DA MEGA 2024/2025 <br> Concurso 2810
             </span>
         </div>
 
